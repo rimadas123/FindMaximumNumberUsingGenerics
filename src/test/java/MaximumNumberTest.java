a2d4c2d296db9bae4ffc5d16e9e7fd235bcc005d
rimadas123 <rdas5969@gmail.com>
2020-03-16 17:11:45 +0530

    Added : pass three integers and calculate maximum number

--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -1,3 +1,13 @@
+import com.generics.MaximumNumber;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MaximumNumberTest {
 
+    MaximumNumber maxNumber = new MaximumNumber();
+    @Test
+    public void givenThreeIntegers_FindMaximumNumber(){
+        Integer result=maxNumber.maxNumberInteger(25,89,56);
+        Assert.assertEquals((Integer)89,result);
+    }
 }
