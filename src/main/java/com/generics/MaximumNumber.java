d4aebca164c2afb90578d26b2f7817311098ad4b
rimadas123 <rdas5969@gmail.com>
2020-03-16 17:23:40 +0530

    Added : three floats and find maximum among them

--- src/main/java/com/generics/MaximumNumber.java
+++ src/main/java/com/generics/MaximumNumber.java
@@ -1,11 +1,27 @@
 package com.generics;
 
 public class MaximumNumber {
+
+    // Welcome message
     public static void main(String[] args) {
         System.out.println("Welcome to find maximum number using generics");
     }
+
+    // Calculate maximum number of an integer
     public Integer maxNumberInteger(Integer firstNumber,Integer secondNumber,Integer thirdNumber){
         Integer max=firstNumber;
+        if(secondNumber.compareTo(max)>0){
+            max=secondNumber;
+        }
+        if(thirdNumber.compareTo(max)>0){
+            max=thirdNumber;
+        }
+        return max;
+    }
+
+    //Calculate maximum number of floats
+    public Double maxNumberFloat(Double firstNumber,Double secondNumber,Double thirdNumber){
+        Double max=firstNumber;
         if(secondNumber.compareTo(max)>0){
             max=secondNumber;
         }
