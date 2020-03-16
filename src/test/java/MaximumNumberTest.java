03b184746f38cb9a559fd56d87475c4f5164a9e8
rimadas123 <rdas5969@gmail.com>
2020-03-16 17:43:43 +0530

    Added : pass three string values and return maximum among them

--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -16,4 +16,10 @@ public class MaximumNumberTest {
         Double result=maxNumber.maxNumberFloat(34.3,56.7,98.6);
         Assert.assertEquals((Double) 98.6,result);
     }
+
+    @Test
+    public void givenThreeStrings_FindMaximum(){
+        String result=maxNumber.maxString("Hello","Hi","world");
+        Assert.assertEquals("world",result);
+    }
 }
