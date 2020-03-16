3e6b94ab2c3c2967416edb399501851d5276d71b
rimadas123 <rdas5969@gmail.com>
2020-03-16 18:16:19 +0530

    Added : pass one generic method to three different test cases

--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -7,19 +7,19 @@ public class MaximumNumberTest {
     MaximumNumber maxNumber = new MaximumNumber();
     @Test
     public void givenThreeIntegers_FindMaximumNumber(){
-        Integer result=maxNumber.maxNumberInteger(25,89,56);
+        Integer result=maxNumber.findMaximum(25,89,56);
         Assert.assertEquals((Integer)89,result);
     }
 
     @Test
     public void givenThreeFloats_FindMaximumNumber(){
-        Double result=maxNumber.maxNumberFloat(34.3,56.7,98.6);
+        Double result=maxNumber.findMaximum(34.3,56.7,98.6);
         Assert.assertEquals((Double) 98.6,result);
     }
 
     @Test
     public void givenThreeStrings_FindMaximum(){
-        String result=maxNumber.maxString("Hello","Hi","world");
+        String result=maxNumber.findMaximum("Hello","Hi","world");
         Assert.assertEquals("world",result);
     }
 }
