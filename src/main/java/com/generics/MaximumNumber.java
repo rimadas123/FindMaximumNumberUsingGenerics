2450d7ae075324901fd3719cee719ddd309b8da1
rimadas123 <rdas5969@gmail.com>
2020-03-17 10:29:13 +0530

    Refactored : to create generic class to take in 3 variables of generic type

--- src/main/java/com/generics/MaximumNumber.java
+++ src/main/java/com/generics/MaximumNumber.java
@@ -1,14 +1,30 @@
 package com.generics;
 
-public class MaximumNumber {
+public class MaximumNumber <E extends Comparable<E>> {
 
     // Welcome message
     public static void main(String[] args) {
         System.out.println("Welcome to find maximum number using generics");
     }
 
-    //given three arguments and compute maximum among them
-    public static <E extends Comparable<E>> E findMaximum(E first,E second,E third){
+    // variables
+    private E first;
+    private E second;
+    private E third;
+    private E testMaximum;
+
+    // default constructor
+    public MaximumNumber(){};
+
+    // parameterized constructor
+    public MaximumNumber(E first,E second,E third){
+        this.first=first;
+        this.second=second;
+        this.third=third;
+    }
+
+    //given three arguments and compute maximum among them using generic method
+    public static <E extends Comparable<E>> E findMaximum(E first, E second, E third){
         E max=first;
         if(second.compareTo(max)>0){
             max=second;
@@ -17,5 +33,11 @@ public class MaximumNumber {
             max=third;
         }
         return max;
+    }
+
+    //method that call another method internally
+    public E findMaximum(){
+        testMaximum =findMaximum(first,second,third);
+        return testMaximum;
     }
 }
